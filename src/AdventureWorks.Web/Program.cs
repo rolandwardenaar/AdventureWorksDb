14251d4656b5bf343751e10ba6c71ddcdcf69ca3
rolandwardenaar <roland.wardenaar@gmail.com>
2025-06-12 15:18:27 +0200

    Fase 2.1 voltooid: Entity Framework setup en database scaffolding - 80+ entities gegenereerd van AdventureWorks2022

--- src/AdventureWorks.Web/Program.cs
+++ src/AdventureWorks.Web/Program.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using AdventureWorks.Web.Data;
+using AdventureWorks.Infrastructure.Data;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add Entity Framework
+builder.Services.AddDbContext<AdventureWorksDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Temporary service - zal later vervangen worden
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
